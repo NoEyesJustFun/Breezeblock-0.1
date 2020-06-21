c239a8ca572fdd8e75d73fdf5350b710274898f9
NoEyesJustFun <4131404+NoEyesJustFun@users.noreply.github.com>
2020-06-21 11:08:33 -0700

    Delete config.h

--- brz60/promicro/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "config_common.h"
-
-
-// Matrix size
-
-#define MATRIX_ROW_PINS { D1, D0, D4, C6, D7 }
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B6, F0, F1, C7, D5, B7, B5, B4, E6 }
-#define UNUSED_PINS { B0, B3, B2 }
