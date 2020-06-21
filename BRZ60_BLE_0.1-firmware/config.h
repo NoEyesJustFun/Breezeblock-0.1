ea8aa3b193719d55001c7f98daef9648890c9729
NoEyesJustFun <4131404+NoEyesJustFun@users.noreply.github.com>
2020-06-21 11:11:24 -0700

    Add files via upload

---
+++ BRZ60_BLE_0.1-firmware/config.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "config_common.h"
+
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6061
+#define DEVICE_VER      0x0001
+#define MANUFACTURER 	NoEyesJustFun
+#define PRODUCT 		Breezeblock
+#define DESCRIPTION 	60 % Bluetooth Equal Staggered Keyboard
+
+// Matrix size
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 12
+
+#define DIODE_DIRECTION ROW2COL                
---
+++ BRZ60_BLE_0.1-firmware/feather/config.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "config_common.h"
+
+
+/* Breezeblock default pinout */
+#define MATRIX_ROW_PINS { F1, F4, F5, F6, F7 }
+#define MATRIX_COL_PINS { F0, D2, D3, C7, D6, B7, B6, B5, D7, C6, D0, D1 }
+#define UNUSED_PINS
+
+#define AdafruitBleResetPin D4
+#define AdafruitBleCSPin B4
+#define AdafruitBleIRQPin E6
